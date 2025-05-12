8e5ff9f8514a37d8142e24826003e71724c70cd4
Luca-Orabona <lucaorabona94@gmail.com>
2025-05-12 15:53:41 +0200

    feat: javascript added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
+const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
+
+const rowElem = document.querySelector(".row");
+
+// Chiamata axios
+axios.get(apiUrl).then((resp) => {
+    const cards = resp.data;
+    
+    let listCards = "";
+    cards.forEach((card) => {
+
+        const {url, date, title} = card;
+        listCards += 
+                    `<div class="col">
+                        <div class="card b-white">
+                            <img class="pin" src="./img/pin.svg" alt="">
+                            <figure>
+                            <img src="${url}" alt="">  
+                            <figcaption>${date}</figcaption>
+                            </figure>       
+                            <h2 class="pt-5">${title}</h2>
+                        </div>
+                    </div>`  
+    })
+
+    rowElem.innerHTML = listCards;
+    
+})
+
 
