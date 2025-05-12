58303f92f3620c19a923a3c455c34148d8e6fe70
Luca-Orabona <lucaorabona94@gmail.com>
2025-05-12 16:42:25 +0200

    refactor: layout + javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
 
+
+
+// funzione concatenamento card
+function listCardsTemplate(card) {
+    const {url, date, title} = card;
+
+    return `<div class="col">
+                <div class="card b-white">
+                    <img class="pin" src="./img/pin.svg" alt="">
+                    <figure>
+                    <img src="${url}" alt="">  
+                    <figcaption class="date">${date}</figcaption>
+                    </figure>       
+                    <h2 class="pt-5">${title}</h2>
+                </div>
+            </div>`  
+}
+
+// Elemento del DOM
 const rowElem = document.querySelector(".row");
 
 // Chiamata axios
@@ -8,23 +27,10 @@ axios.get(apiUrl).then((resp) => {
     
     let listCards = "";
     cards.forEach((card) => {
-
-        const {url, date, title} = card;
-        listCards += 
-                    `<div class="col">
-                        <div class="card b-white">
-                            <img class="pin" src="./img/pin.svg" alt="">
-                            <figure>
-                            <img src="${url}" alt="">  
-                            <figcaption>${date}</figcaption>
-                            </figure>       
-                            <h2 class="pt-5">${title}</h2>
-                        </div>
-                    </div>`  
+        listCards += listCardsTemplate(card)
     })
 
     rowElem.innerHTML = listCards;
     
 })
-
 
