1fd4c448fff97e04b0a397c12b6a95b854eec42c
Luca-Orabona <lucaorabona94@gmail.com>
2025-05-13 17:00:01 +0200

    feat: overlay img added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
 
 // funzione concatenamento card
 function listCardsTemplate(card) {
-    const {url, date, title} = card;
+    const { url, date, title } = card;
 
     return `<div class="col">
                 <div class="card b-white">
@@ -15,22 +15,55 @@ function listCardsTemplate(card) {
                     </figure>       
                     <h2 class="py-5">${title}</h2>
                 </div>
-            </div>`  
+            </div>`
+}
+
+// funzione per ricevere le IMMAGINI della CARD nelle IMMAGINI dell'OVERLAY
+function fimgOverLay(singleCard) {
+    singleCard.addEventListener("click", function () {
+        overLay.classList.remove("d-none");
+        const cardImgElem = singleCard.querySelector("figure img");
+
+        imgOverLay.src = cardImgElem.src;
+
+    })
 }
 
 // Elemento del DOM
 const rowElem = document.querySelector(".row");
+const overLay = document.querySelector(".overlay");
+const btnOverLay = document.querySelector(".overlay button");
+const imgOverLay = document.querySelector(".overlay img");
+
+
 
 // Chiamata axios
 axios.get(apiUrl).then((resp) => {
     const cards = resp.data;
-    
+
+
     let listCards = "";
     cards.forEach((card) => {
         listCards += listCardsTemplate(card)
     })
 
     rowElem.innerHTML = listCards;
-    
+
+    const cardElem = document.querySelectorAll(".card");
+
+    cardElem.forEach((curCard) => {
+        fimgOverLay(curCard)
+    })
+
+
+})
+
+
+
+// bottone di chiusura overlay
+btnOverLay.addEventListener("click", function () {
+
+    overLay.classList.add("d-none");
+
 })
 
